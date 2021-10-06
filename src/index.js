79ab688db1338b1bfb3de30ff60911772684ca37
Dico Diaz <dicodiaz@gmail.com>
2021-10-05 20:33:38 -0500

    Add about text and portfolio pictures

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import Close from './img/close-white.png';
 import Instagram from './img/instagram-white.png';
 import Menu from './img/menu-white.png';
+import picture1 from './img/picture (1).jpeg';
+import picture10 from './img/picture (10).jpeg';
+import picture11 from './img/picture (11).jpeg';
+import picture12 from './img/picture (12).jpeg';
+import picture2 from './img/picture (2).jpeg';
+import picture3 from './img/picture (3).jpeg';
+import picture4 from './img/picture (4).jpeg';
+import picture5 from './img/picture (5).jpeg';
+import picture6 from './img/picture (6).jpeg';
+import picture7 from './img/picture (7).jpeg';
+import picture8 from './img/picture (8).jpeg';
+import picture9 from './img/picture (9).jpeg';
 import TikTok from './img/tik-tok-white.png';
 import './style.css';
 
@@ -44,10 +56,23 @@ const iconInstagram = document.querySelector('#icon-instagram');
 const iconTikTok = document.querySelector('#icon-tiktok');
 const menuContainer = document.querySelector('#menu-container');
 const menuItems = document.querySelectorAll('#menu > a');
+const picturesContainer = document.querySelector('#pictures');
 
 appendImg(Instagram, iconInstagram);
 appendImg(TikTok, iconTikTok);
 appendCrossFadingImgs(Menu, Close, menuContainer);
+appendImg(picture1, picturesContainer);
+appendImg(picture2, picturesContainer);
+appendImg(picture3, picturesContainer);
+appendImg(picture4, picturesContainer);
+appendImg(picture5, picturesContainer);
+appendImg(picture6, picturesContainer);
+appendImg(picture7, picturesContainer);
+appendImg(picture8, picturesContainer);
+appendImg(picture9, picturesContainer);
+appendImg(picture10, picturesContainer);
+appendImg(picture11, picturesContainer);
+appendImg(picture12, picturesContainer);
 
 menuContainer.addEventListener('click', () => {
   menuContainer.firstChild.childNodes.forEach((child) => {
