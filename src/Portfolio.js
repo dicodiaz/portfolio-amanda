bfd87a652ab1ceaf7d53beb4bc0c7099acbe7cb0
Dico Diaz <dicodiaz@gmail.com>
2021-10-10 19:32:44 -0500

    Fix styling & portfolio section & add carousel
    * Add font awesome icons
    * Remove local icons
    * Fix portfolio section images, now every consecutive image loads horizontally until next line
    * Every image next line renders just under the last one, independently of the column
    * Refactor image appending into Portfolio class
    * Add carousel to commercial section

---
+++ src/Portfolio.js
@@ -0,0 +1,58 @@
+export default class Portfolio {
+  constructor({ pictures = [] } = {}) {
+    this.pictures = pictures;
+  }
+
+  addPicture = (picture) => {
+    this.pictures.push(picture);
+    return this;
+  };
+
+  addAllPictures = (pictures) => {
+    pictures.forEach((picture) => this.addPicture(picture));
+    return this;
+  };
+
+  appendPicture = (picture, container, x = -1, y = -1) => {
+    const img = document.createElement('img');
+    img.src = picture;
+    img.alt = picture;
+    img.classList.add('p-1', 'img-fluid');
+    if (x !== -1 && y !== -1) {
+      img.classList.add('w-50', 'position-absolute');
+      img.style = `top: ${y}px; left: ${x}px`;
+      img.dataset.toggle = 'modal';
+      img.dataset.target = '#modal';
+    }
+    container.appendChild(img);
+    return new Promise((resolve) => (img.onload = resolve));
+  };
+
+  appendAllPictures = async (container) => {
+    container.classList.add('position-relative');
+    const width = container.offsetWidth,
+      seeMoreContainer = document.createElement('div');
+    let yCol1 = 0,
+      yCol2 = 0,
+      relevantContainer = container;
+    for (let i = 0; i < this.pictures.length; i++) {
+      if (i === 4) {
+        seeMoreContainer.id = 'collapsable';
+        // seeMoreContainer.classList.add('collapse');
+        container.appendChild(seeMoreContainer);
+        relevantContainer = seeMoreContainer;
+      }
+      if (i % 2 === 0) {
+        await this.appendPicture(this.pictures[i], relevantContainer, 0, yCol1);
+        yCol1 += container.querySelectorAll('img')[i].offsetHeight;
+      } else {
+        await this.appendPicture(this.pictures[i], relevantContainer, width / 2, yCol2);
+        yCol2 += container.querySelectorAll('img')[i].offsetHeight;
+      }
+    }
+    container.style.height = Math.max(yCol1, yCol2) + 'px';
+    return this;
+  };
+
+  delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+}
